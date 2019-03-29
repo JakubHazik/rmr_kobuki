3e5f9f176cd67c733b58dc6b0fc263f286ee7701
Jakub Hazik <hazik.jakub@gmail.com>
2019-03-29 23:37:48 +0100

    kobuki visualizer is prepaired

--- kobuki_project/include/visualizer.h
+++ kobuki_project/include/visualizer.h
@@ -1,6 +1,28 @@
 //
 // Created by jakub on 28.3.2019.
 //
+/*
+ Usage:
+    RobotMap map(RobotPose{6000 * 2, 6000 * 2}, 30);
+    map.loadIdealMap("../ideal_map", RobotPose{5300, 600, 180});
+
+    int robotWidth = 260; //mm
+    RobotPose odom = {0,0,45};
+    RobotPose goal = {5000, -1600, 0};
+    GlobalPlanner gPlanner(map.getRobotMap(), odom, goal, robotWidth);
+
+    Visualizer visualizer(map.getSize(), robotWidth, map.getResolution());
+    visualizer.environmentMap = map.getCVMatMap();
+    visualizer.path = gPlanner.getPathImage();
+    visualizer.waypoints = gPlanner.getWayPointsImage();
+    visualizer.floodFill = gPlanner.getFloodFillImage();
+
+    auto img = visualizer.getImage(odom);
+
+    cv::resize(img, img, cv::Size(600,600));
+    cv::imshow("a", img);
+    cv::waitKey(0);
+ */
 
 #ifndef KOBUKI_PROJECT_VISUALIZER_H
 #define KOBUKI_PROJECT_VISUALIZER_H
@@ -9,60 +31,48 @@
 #include "include/own_typedefs.h"
 #include <opencv2/opencv.hpp>
 
-/*
- * prazdny priestor biela
- * steny cierna
- * waipoints modra
- * robot crevena
- * floodfill zlta
- * path zelena
- *
- *
- */
 
 // COLOR VALUES Scalar(Blue, Green, Red)
-#define COLOR_WHITE Scalar(255,255,255)
-#define COLOR_RED Scalar(0,0,255)
-#define COLOR_GREEN Scalar(0,255,0)
-#define COLOR_BLUE Scalar(255,0,0)
+#define COLOR_BACKGROUND Scalar(255,255,255)    //white
+#define COLOR_ROBOT Scalar(0,0,255)             //red
+#define COLOR_PATH Scalar(0,255,0)              //green
+#define COLOR_WAYPOINTS Scalar(255,0,0)         //blue
+#define COLOR_FLOOD_FILL Scalar(20,140,100)     //yellow
 #define COLOR_BLACK Scalar(1,1,1)
-
 
 class Visualizer {
 public:
     Visualizer(MapSize mapSize, int robotWidth, int mapResolution);
 
-    cv::Mat getImage(RobotPose robotPose);
-    cv::Mat getImage(RobotPose robotPose, RobotMap laserData);
-    cv::Mat getImage(RobotPose robotPose, RobotMap environmentMap, RobotMap laserData);
+    cv::Mat getImage(RobotPose robotPose);                      // TODO nemusime vracat Mat, mozeme rovno QCanvas
+    cv::Mat getImage(RobotPose robotPose, RobotMap laserData);  //TODO implementovat podla svojej lubovole
 
-    RobotPose robotPose = {};
-    RobotPose goalPose = {};
     cv::Mat environmentMap;
     cv::Mat path;
     cv::Mat waypoints;
     cv::Mat floodFill;
 private:
-    void addRobot(cv::Mat &inputOutput, RobotPose robotPose, cv::Scalar color);
-
-    void addFloodFill(cv::Mat &output, cv::Mat &addImg);
-
-//    cv::Mat robotImage;
-
-    void addImageMask(cv::Mat &output, cv::Mat &addImg, cv::Scalar color);
-
-    void convertImage(cv::Mat &img);
-
     MapSize mapSize;
-
-    cv::Mat robotImage, robotImageMask;
-
     int mapResolution;
     int robotWidth;
 
+    /*
+     * Draw robot to image
+     */
+    void addRobot(cv::Mat &inputOutput, RobotPose robotPose, cv::Scalar color);
 
+    /*
+     * Draw flood fill to image
+     */
+    void addFloodFill(cv::Mat &output, cv::Mat &addImg);
 
-
+    /**
+     * Add a image to output image as next no transparency layer
+     * @param output output image
+     * @param addImg image which we want to add, this Mat can have only <0,1> values
+     * @param color color
+     */
+    void addImageMask(cv::Mat &output, cv::Mat &addImg, cv::Scalar color);
 };
 
 
--- kobuki_project/src/visualizer.cpp
+++ kobuki_project/src/visualizer.cpp
@@ -14,36 +14,38 @@ Visualizer::Visualizer(MapSize mapSize, int robotWidth, int mapResolution) {
 
 cv::Mat Visualizer::getImage(RobotPose robotPose) {
     Mat output = cv::Mat(mapSize.x, mapSize.y, CV_8UC3);
-    output.setTo(COLOR_WHITE);         // set image to white color
+    output.setTo(COLOR_BACKGROUND);         // set image to white color
 
     if (!environmentMap.empty()) {
         addImageMask(output, environmentMap, COLOR_BLACK);
     }
 
+    if (!floodFill.empty()) {
+        addFloodFill(output, floodFill);
+    }
+
     if (!path.empty()) {
-        addImageMask(output, path, COLOR_GREEN);
+        addImageMask(output, path, COLOR_PATH);
     }
 
     if (!waypoints.empty()) {
-        addImageMask(output, waypoints, COLOR_BLUE);
+        addImageMask(output, waypoints, COLOR_WAYPOINTS);
     }
 
-    addRobot(output, robotPose, COLOR_RED);
+    addRobot(output, robotPose, COLOR_ROBOT);
 
     return output;
 }
 
 
 void Visualizer::addRobot(cv::Mat &inputOutput, RobotPose robotPose, cv::Scalar color) {
-
     double fi = robotPose.fi;
-    int x = (int)round((cos(fi*DEG2RAD)) * robotWidth/mapResolution);
-    int y = (int)round((sin(fi*DEG2RAD)) * robotWidth/mapResolution);
+    int x = (int)round((cos(fi*DEG2RAD)) * robotWidth/2/mapResolution);
+    int y = (int)round((sin(fi*DEG2RAD)) * robotWidth/2/mapResolution);
 
     MapPoint robot = RobotMap::tfRealToMap(robotPose, mapSize, mapResolution);
-    circle(inputOutput, Point(robot.x, robot.y), robotWidth/mapResolution, color);
+    circle(inputOutput, Point(robot.x, robot.y), robotWidth/2/mapResolution, color);
     arrowedLine(inputOutput, Point(robot.x, robot.y), Point(robot.x + x, robot.y + y), color);
-
 }
 
 
@@ -54,40 +56,21 @@ void Visualizer::addImageMask(cv::Mat &output, cv::Mat &addImg, cv::Scalar color
 }
 
 void Visualizer::addFloodFill(Mat &output, Mat &addImg) {
-    Mat mask, mask_inv,img1Gray;
-
-    cvtColor(addImg, img1Gray, COLOR_RGB2GRAY);
-    threshold(img1Gray, mask, 1, 255, cv::THRESH_BINARY);
+    Mat mask, mask_inv;
+    addImg.convertTo(addImg, CV_8UC3);
+    threshold(addImg, mask, 1, 255, cv::THRESH_BINARY);
     bitwise_not(mask, mask_inv);
 
-//    cv::imshow("a", output);
-//    cv::waitKey(0);
-//    cv::imshow("a", addImg);
-//    cv::waitKey(0);
-//
-//    cv::imshow("mask", mask);
-//    cv::waitKey(0);
-//    cv::imshow("mask_inv", mask_inv);
-//    cv::waitKey(0);
-
-
     Mat output_bg, addImg_bg;
-
+    cvtColor(addImg, addImg, COLOR_GRAY2RGB);
     bitwise_and(output, output, output_bg, mask_inv);
     bitwise_and(addImg, addImg, addImg_bg, mask);
 
-//    cv::imshow("output_bg", output_bg);
-//    cv::waitKey(0);
-//    cv::imshow("addImg_bg", addImg_bg);
-//    cv::waitKey(0);
+    Mat color = Mat(addImg_bg.size(), CV_8UC3, COLOR_BLACK);
+    color.setTo(COLOR_FLOOD_FILL, mask);
 
+    addImg_bg += color;
     output = output_bg + addImg_bg;
-
-//    cv::imshow("result", output);
-//    cv::waitKey(0);
-
-
-//    retur?n Mat();
 }
 
 
